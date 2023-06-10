2b05262cebd090fe299256abbc81c984aae2460f
John Roller <jtroller33@gmail.com>
2023-06-10 13:38:58 -0500

    added font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <style jsx global>{`
+      html {
+        font-family: ${roboto.style.fontFamily};
+      }
+      `}</style>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
