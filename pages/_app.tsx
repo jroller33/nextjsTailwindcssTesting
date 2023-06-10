25c1c7ccd1e45da1edab156245b47fe970963b1d
John Roller <jtroller33@gmail.com>
2023-06-10 13:48:43 -0500

    datePicker component from  is working correctly. Project setup seems to be working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { Roboto } from "next/font/google";
+import "tw-elements/dist/css/tw-elements.min.css";
+
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
