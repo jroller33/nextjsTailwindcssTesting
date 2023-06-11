7f8a6146708bbd16be73e408d9c60e6c47d147c0
John Roller <jtroller33@gmail.com>
2023-06-11 14:57:05 -0500

    removed tw-elements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 import { Roboto } from "next/font/google";
-import "tw-elements/dist/css/tw-elements.min.css";
-
-
-const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 
 export default function App({ Component, pageProps }: AppProps) {
+
+
   return (
     <div>
-      <style jsx global>{`
-      html {
-        font-family: ${roboto.style.fontFamily};
-      }
-      `}</style>
+
 
       <Component {...pageProps} />
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import dynamic from "next/dynamic";
-import MyCarousel from "../components/MyCarousel";
 
-const DynamicCarousel = dynamic(() => import("../components/MyCarousel"), { ssr: false });
+
+
+// const DynamicCarousel = dynamic(() => import("../components/MyCarousel"), { ssr: false });
 // const DynamicDatepicker = dynamic(() => import("../components/datePicker"), { ssr: false });
 
 const Home = () => {
+
   return (
     <div>
-
-      <MyCarousel />
-
+      
     </div>
   );
 };
