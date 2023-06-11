cab63ba2def87207c4a820a1897cc934fd04742a
John Roller <jtroller33@gmail.com>
2023-06-11 15:00:54 -0500

    redid tailwind config for custom Carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,9 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  darkMode: "class",
-  plugins: [require("tw-elements/dist/plugin.cjs")]
+  plugins: []
 }
