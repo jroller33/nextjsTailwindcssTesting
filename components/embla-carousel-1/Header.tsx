42658ec96c1ac93019444f59f7e6713d73756a46
John Roller <jtroller33@gmail.com>
2023-06-11 18:07:09 -0500

    done with embla-carousel for now. they used vanilla CSS instead of tailwind, so the CSS isn

---
+++ components/embla-carousel-1/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header: React.FC = () => (
+  <header>
+    <h1 className="sandbox__header">Embla Carousel Arrow Dots React</h1>
+  </header>
+)
+
+export default Header
